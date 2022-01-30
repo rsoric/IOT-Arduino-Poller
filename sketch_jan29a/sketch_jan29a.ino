40c3a64b8307fe8ed82b2bf4b6d08328b08880e8
rsoric <robert.soric@gmail.com>
2022-01-30 18:29:42 +0100

    Sending to mysql database works

--- sketch_jan29a/sketch_jan29a.ino
+++ sketch_jan29a/sketch_jan29a.ino
@@ -2,10 +2,31 @@
 #include <RBD_Button.h>
 #include <ESP8266WiFi.h>
 
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 #ifndef STASSID
 #define STASSID "PAP"
 #define STAPSK  "pap13789"
 #endif
+
+String question1 = "Hrana?";
+String question2 = "Cijena?";
+String question3 = "Usluga?";
+
+String reply1 = "Uzas";
+String reply2 = "Nije dobro";
+String reply3 = "OK";
+String reply4 = "Solidno";
+String reply5 = "Genijalno";
+
+int q1ValueToSend = 0;
+int q2ValueToSend = 0;
+int q3ValueToSend = 0;
+
 
 const char* ssid     = STASSID;
 const char* password = STAPSK;
@@ -13,13 +34,17 @@ const char* password = STAPSK;
 const char* host = "iotrisevi.herokuapp.com";
 const uint16_t port = 80;
 String PATH_NAME   = "/insert-response.php";
-String queryString = "?q1=29.1&q2=55&q3=2.525";
+
 String HTTP_METHOD = "GET";
+
 
 RBD::Button button(12);
 
 void setup() {
   Serial.begin(115200);
+
+  lcd.begin();
+  lcd.backlight();
 
   // We start by connecting to a WiFi network
 
@@ -47,13 +72,54 @@ void setup() {
 
 void loop() {
 
-if(button.onPressed()) {
-    sendData();
+lcd.setCursor(0,0);
+lcd.print(question1);
+while(true)
+{
+  delay(50);
+  int q1Value = analogRead(A0);
+  lcd.setCursor(0,1);
+  lcd.print(q1Value);
+  if(button.onPressed()) {
+    q1ValueToSend = q1Value; 
+    break;    
   }
+}
+
+lcd.setCursor(0,0);
+lcd.print(question2);
+while(true)
+{
+  delay(50);
+  int q2Value = analogRead(A0);
+  lcd.setCursor(0,1);
+  lcd.print(q2Value);
+  if(button.onPressed()) {
+    q2ValueToSend = q2Value; 
+    break;    
+  }
+}
+
+lcd.setCursor(0,0);
+lcd.print(question3);
+while(true)
+{
+  delay(50);
+  int q3Value = analogRead(A0);
+  lcd.setCursor(0,1);
+  lcd.print(q3Value);
+  if(button.onPressed()) {
+    q3ValueToSend = q3Value; 
+    break;    
+  }
+}
+
+sendData(q1ValueToSend,q2ValueToSend,q3ValueToSend);
+
 
 }
 
-void sendData(){
+void sendData(int q1ValueToSend, int q2ValueToSend, int q3ValueToSend){
   
   Serial.print("connecting to ");
   Serial.print(host);
@@ -70,8 +136,13 @@ void sendData(){
 
   // This will send a string to the server
   Serial.println("sending data to server");
+  //String queryString = "?q1=29.1&q2=55&q3=2.525";
   if (client.connected()) {
-    client.println(HTTP_METHOD + " " + PATH_NAME + queryString + " HTTP/1.1");
+    client.println(HTTP_METHOD + " " + PATH_NAME + 
+    "?q1="+String(q1ValueToSend)
+    +"&q2="+String(q2ValueToSend)
+    +"&q3="+String(q3ValueToSend) 
+    + " HTTP/1.1");
     client.println("Host: " + String(host));
     client.println("Connection: close");
     client.println(); // end HTTP header
