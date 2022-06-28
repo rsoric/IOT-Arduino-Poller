e6feed18305fbd104d7eb814c5526af46d6d9708
davidhod1 <davidhodak225@gmail.com>
2022-06-28 22:34:09 +0200

    Added fronted for inserting question

--- dashboard_database.php
+++ dashboard_database.php
@@ -2,7 +2,32 @@
 
 <div class="container-fluid dashboard-content">
 
-    <h1>Database manipulation!</h1>
+    <div class="row">
+        <div class="col">
+            <h1>Add question:</h1>
+        </div>
+        <div class="col-3">
+            <form action="dashboard-releases.php">
+                <input type="submit" id="back-button" value="Go back">
+            </form>
+        </div>
+    </div>
+
+    <div class="new-question-input">
+        <form id="edit-question-form" action="Backend/question_add.php" method="post">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="form-group">
+                        <label for="questionText">Question:</label>
+                        <input class="form-control form-control-lg" type="text" name= "questionText" required>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex p-2 justify-content-around">
+                <input type="submit" name="addQuestion" class="btn btn-primary add-question-button" value="Save">   
+            </div>
+        </form>
+    </div>
 
 </div>
 
