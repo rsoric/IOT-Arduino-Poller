5dc02d9d43961d29ecbf8c16bd6da8edfac0536d
davidhod1 <davidhodak225@gmail.com>
2022-06-29 19:51:41 +0200

    Fixed

--- dashboard_add_questions.php
+++ dashboard_add_questions.php
@@ -4,10 +4,10 @@
 
     <div class="row">
         <div class="col">
-            <h1>Add question:</h1>
+            <h2>Add question:</h2>
         </div>
         <div class="col-3">
-            <form action="dashboard-releases.php">
+            <form action="dashboard_database.php">
                 <input type="submit" id="back-button" value="Go back">
             </form>
         </div>
@@ -15,15 +15,15 @@
 
     <div class="new-question-input">
         <form id="edit-question-form" action="Backend/questions_add.php" method="post">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
+            <div class="row justify-content-left">
+                <div class="col-md-4">
                     <div class="form-group">
-                        <label for="questionText">Question text:</label>
-                        <input class="form-control form-control-lg" type="text" name= "questionText" required>
+                        <label for="questionText">Question:</label>
+                        <input class="form-control form-control-lg" type="text" name= "questionText" maxlength="16" required>
                     </div>
                 </div>
             </div>
-            <div class="d-flex p-2 justify-content-around">
+            <div class="d-flex p-2 justify-content-left">
                 <input type="submit" name="addQuestion" class="btn btn-primary add-question-button" value="Save">   
             </div>
         </form>
