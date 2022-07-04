24d9ab35f0acf9cee2836d69224a4e2a74ddd371
rsoric <robert.soric@gmail.com>
2022-07-04 14:32:23 +0200

    Update currentPoll_edit.php

--- Backend/currentPoll_edit.php
+++ Backend/currentPoll_edit.php
@@ -10,6 +10,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $currentPoll = sanitizeInput($_POST["currentPollId"]);
 
     if (isset($_POST['update'])) {
+
+        error_reporting(E_ALL);
+        ini_set('display_errors', 'on');
         
         try{
             $_currentPoll->updateCurrentPoll($currentPoll);
