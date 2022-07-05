07c050d4441366055522fe2ec2593654a4217a7e
davidhod1 <davidhodak225@gmail.com>
2022-07-05 19:20:27 +0200

    Sheeesh

---
+++ Backend/getReplies.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+table, td, th {
+  border: 1px solid black;
+  padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
+<?php
+$q = intval($_GET['q']);
+
+$servername = "eu-cdbr-west-02.cleardb.net";
+$username = "b8100c5581c24b";
+$password = "2ab80845";
+$dbname = "heroku_526e4c652212ab8";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+
+$sql="SELECT * FROM question_replies 
+INNER JOIN polls
+ON question_replies.pollId = polls.pollId
+WHERE polls.pollId = '".$q."'";
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+$result = $conn->query($sql);
+
+echo "<table>
+<tr>
+<th>Value</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['Value'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+mysqli_close($con);
+?>
+</body>
+</html>
